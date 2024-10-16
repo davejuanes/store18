73cfc827cf6c0d1b00ea4ff00fd44fa039e68c3d
DAVID MERCADO JUANES <dmercado@abc.gob.bo>
2024-10-16 12:14:26 -0400

    [CLASE 43] CREANDO HEADER Y DIMENSIONANDO LOS ESPACIOS

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../../shared/models/product.mode';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -15,6 +16,27 @@ export class ListComponent {
 
   constructor() {
     const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Prod 1',
+        price: 1000,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Prod 2',
+        price: 3000,
+        image: 'https://picsum.photos/640/640?r=21',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Prod 3',
+        price: 7000,
+        image: 'https://picsum.photos/640/640?r=28',
+        creationAt: new Date().toISOString()
+      },
       {
         id: Date.now(),
         title: 'Prod 1',
