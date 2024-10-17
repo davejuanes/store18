23e711fdbccb88f916df82ff4f20cdfcb6223b4e
DAVID MERCADO JUANES <dmercado@abc.gob.bo>
2024-10-17 18:58:14 -0400

    [CLASE 65] MOSTRANDO CATEGORIES DESDE LA API

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -5,6 +5,8 @@ import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
@@ -15,10 +17,21 @@ import { ProductService } from '@shared/services/product.service';
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService)
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit() {
+    this.getProducts()
+    this.getCategories()
+  }
+
+  addToCart(product: Product) { // Se necesita para poder recibir los eventos de los componentes hijos
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
     this.productService.getProducts()
     .subscribe({
       next: (products) => {
@@ -29,9 +42,16 @@ export class ListComponent {
       }
     })
   }
+  private getCategories() {
+    this.categoryService.getCategories()
+    .subscribe({
+      next: (categories) => {
+        this.categories.set(categories);
+      },
+      error: () => {
 
-  addToCart(product: Product) { // Se necesita para poder recibir los eventos de los componentes hijos
-    this.cartService.addToCart(product);
+      }
+    })
   }
 }
 /* Ciclo de vida de componentes
