f6ae201d4c5195469bd4e6240e19852b8f0140b6
DAVID MERCADO JUANES <dmercado@abc.gob.bo>
2024-10-17 18:46:18 -0400

    [CLASE 64] DETALLES DE LA GALERIA

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, signal } from '@angular/core';
 import { Product } from '@shared/models/product.model';
+import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
@@ -15,6 +16,7 @@ export class ProductDetailComponent {
   product = signal<Product | null>(null);
   cover = signal('');
   private productService = inject(ProductService);
+  private cartService = inject(CartService);
 
   ngOnInit() {
     if (this.id) {
@@ -31,5 +33,11 @@ export class ProductDetailComponent {
   }
   changeCover(newImg: string) {
     this.cover.set(newImg)
+  }
+  addToCart() {
+    const product = this.product();
+    if (product) {
+      this.cartService.addToCart(product)
+    }
   }
 }
